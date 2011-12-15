27b089bbb0f8692e191def52f161eca3dd9afc25
Flavio Curella <flavio.curella@gmail.com>
2011-12-15 15:26:04 -0600

    refactored the pythonpath monkeypatch

--- bin/inspect_setting.py
+++ bin/inspect_setting.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
 import os
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__))))
 from settings_inspector.setting import Setting
 
 
@@ -8,5 +9,4 @@ def main(setting_module_path='django.conf'):
     root_setting = Setting(setting_module_path)
 
 if __name__ == "__main__":
-    sys.path.append(os.path.join(os.path.dirname(__file__), 'settings_inspector'))
     main(sys.argv[1])
